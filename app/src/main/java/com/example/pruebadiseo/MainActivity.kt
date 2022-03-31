bc68e4fa7edaffbe5caa48523cb1735e6dcc3ff9
Mario Acedo Caballero <mario.acedo@atmira.com>
2022-03-31 10:44:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pruebadiseo/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.pruebadiseo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.pruebadiseo.databinding.ActivityMainBinding
+import com.example.pruebadiseo.databinding.ActivityMainBinding.inflate
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
